16876360b833f6f1938f38fb281c436edf3240aa
Iuri de Silvio <iurisilvio@gmail.com>
2010-06-05 17:55:29 -0300

    .

--- src/enxuga-enchente-gae/enxuga.py
+++ src/enxuga-enchente-gae/enxuga.py
@@ -5,6 +5,8 @@ import cgi
 import datetime
 import wsgiref.handlers
 
+import simplejson as json
+
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
@@ -17,13 +19,43 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write('Funcionar, funciona')
 
 
-class Guestbook(webapp.RequestHandler):
+class ManyProblemsPage(webapp.RequestHandler):
+    def get(self):
+        pass
+
+
+class OneProblemPage(webapp.RequestHandler):
+    
+    def get(self):
+        return json.JSONEncoder().encode(self, Problem.get(self.request.GET.get("id")))
+
     def post(self):
-        self.redirect('/')
+        problem = Problem(author=self.request.user,
+                          geolocation=self.request.POST.get("geolocation"),
+                          description=self.request.POST.get("description"))
+        problem.put()
+        
+        
+class PhotoPage(webapp.RequestHandler):
+    def get(self):
+        return json.JSONEncoder().encode(self, Photo.get(self.request.GET.get("id")))
+
+    def post(self):
+        image = self.request.POST.get("photo")
+        
+        photo = Photo(problem=self.request.POST.get("problem_id"),
+                      author=users.get_current_user(),
+                      description=self.request.POST.get("description"))
+                      
+        photo.put()
 
 
-application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/sign', Guestbook)], debug=True)
+class CommentPage(webapp.RequestHandler):
+    pass
+        
+application = webapp.WSGIApplication([('/problems', ManyProblemsPage),
+                                      ('/problem', OneProblemPage),
+                                      ('/photo', PhotoPage)], debug=True)
 
 
 def main():
