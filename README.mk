c68119e7e7ab2e91296333f7108be9294fdeb907
Iraê <irae@irae.pro.br>
2010-06-05 12:26:43 -0300

    Commit inicial com README e .gitignore

---
+++ README.mk
@@ -0,0 +1,11 @@
+Enxuga Enchente
+======
+Um aplicativo para rede sociais para ajudar a acabar com as enchentes de São Paulo.
+
+Idéias
+--------
+
+- Usar OpenSocial para criar o front end no Orkut
+- Usuários cadastram fotos, informações, geolocations e tudo que pode ajudar as autoridades a agir
+- Usuários mais ativos são recompensados com sistema de pontuação
+
