fc98ba0b9304c8b4352df62c08c4fffb0589f313
Iuri de Silvio <iurisilvio@gmail.com>
2010-06-05 16:07:55 -0300

    inicio de model

--- src/enxuga-enchente-gae/models.py
+++ src/enxuga-enchente-gae/models.py
@@ -3,8 +3,36 @@
 from google.appengine.ext import db
 from google.appengine.api import users
 
-class Greeting(db.Model):
+    
+class Problem(db.Model):
     author = db.UserProperty()
-    content = db.StringProperty(multiline=True)
     date = db.DateTimeProperty(auto_now_add=True)
+    
+    closed = db.BooleanProperty(default=False)
 
+
+class Photo(db.Model):
+    """Informações da foto do problema"""
+    
+    problem = db.ReferenceProperty(Problem)
+    author = db.UserProperty()
+    
+    db.BlobProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+    
+    geolocation = db.GeoPtProperty()
+    
+    
+class Comment(db.Model):
+    """Comentário relacionado a um problema"""
+    
+    photo = db.ReferenceProperty(Photo)
+    
+    
+class Vote(db.Model):
+    """Voto para um problema"""
+    
+    problem = db.ReferenceProperty(Problem)
+    user = db.UserProperty()
+    vote = db.IntegerProperty()
+    
